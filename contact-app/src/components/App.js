3a7706de2835baaca97622911c963a849c199c3f
pandu-12-2000 <crackivar@gmail.com>
2025-09-24 13:48:38 +0530

    making it dynamic

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,27 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
+
 function App() {
 
-  const contacts=[
-    { id:"1",
-    "name":"pandu",
-    "email":"sul@gmail.com",
-    },
-    {id:"2",
-    "name":"kandu",
-    "email":"sop@gmail.com",
-    },
-        ];
-
+const[contacts, setContacts]=useState([]);
+const addContactHandler=(contact)=>{
+  console.log(contact)
+}
   return (
   <div className="ui conatiner ">
 
 
     <Header/>
-    <AddContact/>
+    <AddContact addContactHandler={addContactHandler}/>
     <ContactList contactsData={contacts}/>
     </div>
   );
